a84fe02ea6e7336574a6b3e5b6463ba7ab246092
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2025-01-08 03:13:26 +0200

    increase width and height of the ping animation

--- src/app/[locale]/_component/header/animatedPing.tsx
+++ src/app/[locale]/_component/header/animatedPing.tsx
@@ -3,7 +3,12 @@ import React from "react";
 
 const AnimatedPing = ({ className }: { className?: string }) => {
   return (
-    <span className={cn("relative flex h-2 w-2", className)}>
+    <span
+      className={cn(
+        "relative h-3 w-3 flex justify-center items-center",
+        className
+      )}
+    >
       <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-ring opacity-75 colors"></span>
       <span className="relative inline-flex rounded-full h-2 w-2 bg-ring"></span>
     </span>
